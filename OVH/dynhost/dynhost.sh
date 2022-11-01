c49368f3a1c184c45d3ab7e9cac5179da35ffb15
Phips02 <Phips01@bluewin.ch>
2022-11-01 19:11:14 +0100

    dynamic_dynhost_V0.2
    Added Telegram notifications

--- OVH/dynhost/dynhost.sh
+++ OVH/dynhost/dynhost.sh
@@ -4,7 +4,33 @@
 # sudo apt-get update
 # sudo apt-get install dnsutils
 
+#Step 2
+# Add on crontab
+# Crontab -l
+# 00 00 * * * /huser/pi/OVH/synhost.sh
+
 source /home/pi/OVH/dynhost.credentials.sh
+
+
+#----------------------------------------------------Telegram----------------------------------------------------
+DATE=$(date "+%F %H:%M:%S")
+IP_PUBLIC=$(curl -s ipinfo.io/ip)
+API="https://api.telegram.org/bot${KEY}"
+
+function telegram_text_send()
+{
+	API="$1"
+	CHATID="$2"
+	KEY="$3"
+	PARSE_MODE="$4"
+	TEXT="$5"
+	ENDPOINT="sendMessage"
+
+	curl -s -d "chat_id=${CHATID}&text=${TEXT}&parse_mode=${PARSE_MODE}" ${API}/${ENDPOINT} >/dev/null
+#	curl -s -d "chat_id=${CHATID}&text=${TEXT}&parse_mode=${PARSE_MODE}" ${API}/${ENDPOINT}
+}
+#----------------------------------------------------------------------------------------------------------------
+
 
 MY_IP=$(curl -s https://ifconfig.me)
 
@@ -37,19 +63,26 @@ function LOG() {
 ###
 
 
+
 if [ -e OLD_IP.txt ]; then
 	LOG "File OLD_IP.txt existing, ok"
 	OLD_IP=$(cat OLD_IP.txt)
 	LOG "Stored external IP address: $OLD_IP"
+        LOG "Current external IP address: $MY_IP"
 else
 	LOG "The file OLD_IP.txt does not exist"
 	LOG "file creation..."
-	echo "$MY_IP" > OLD_IP.txt
+        LOG "Current external IP address: $MY_IP"
+        echo "$MY_IP" > OLD_IP.txt
 fi
 
 
-LOG "Current external IP address: $MY_IP"
-
+#--------------Telegram--------------
+TEXT="$DATE %0A\
+Update public IP address on OVH DynHost %0A\
+OLD IP : $OLD_IP %0A\
+NEW IP : $IP_PUBLIC"
+#------------------------------------
 
 if [ "$MY_IP" = "$OLD_IP" ]; then
         LOG "IP haven't change."
@@ -59,6 +92,7 @@ else
         # Updating dynHost IP
         url="https://www.ovh.com/nic/update?system=dyndns&hostname=$OVH_HOSTNAME&myip=$IP"
 	curl -s $url --user "$OVH_USERNAME:$OVH_PASSWD"
+        telegram_text_send "$API" "$CHATID" "$KEY" "markdown" "$TEXT"
 
 fi
 
@@ -67,3 +101,4 @@ LOG "-------------------------------------------------------"
 
 #mise à jour adresse dans le script
 echo "$MY_IP" > OLD_IP.txt
+
