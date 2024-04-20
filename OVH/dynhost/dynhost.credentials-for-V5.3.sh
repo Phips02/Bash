7dfafae11da7f7cbacc6df8ca642a3dcadb08c97
Phips02 <51390080+Phips02@users.noreply.github.com>
2024-04-20 19:18:00 +0200

    Bug_fix-crontab

---
+++ OVH/dynhost/dynhost.credentials-for-V5.3.sh
@@ -0,0 +1,5 @@
+OVH_HOSTNAME="xxx.xxx.ovh"
+OVH_USERNAME="xxx.ovh-xxx"
+OVH_PASSWD="password"
+TELEGRAM_BOT_TOKEN="123:sdfknsdkfbnkdsbfkbdskjf"
+TELEGRAM_CHAT_ID="123"
--- OVH/dynhost/dynhost.credentials.sh
+++
@@ -1,3 +0,0 @@
-OVH_HOSTNAME="home.mydomain.com"
-OVH_USERNAME="user.ovh-DynHost"
-OVH_PASSWD="********"
